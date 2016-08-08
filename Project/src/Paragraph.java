97bb3dc00f5eae2e2e54ccef3b8b1af940a79d27
Rajesh <srajesh@paypalcorp.com>
2016-08-08 10:01:07 +0530

    paragraph

--- Project/src/Paragraph.java
+++ Project/src/Paragraph.java
@@ -1,4 +1,47 @@
+import java.awt.List;
+import java.util.ArrayList;
 
 public class Paragraph {
-
+	
+	public ArrayList<Sentence> sentences = null;
+	
+	public Paragraph(String paragraph){
+		this.sentences = new ArrayList<Sentence>();
+		String sentence = "";
+		for(char ch: paragraph.toCharArray()){
+			sentence += ch;
+			if(ch=='\n'){
+				this.sentences.add(new Sentence(sentence));
+				sentence = "";
+			}
+		}
+	}
+	
+	public int getNumLines(){
+		return this.sentences.size();
+	}
+	
+	public int getNumWords(){
+		int answer = 0;
+		for(Sentence s: this.sentences){
+			answer += s.getNoWords();
+		}
+		return answer;
+	}
+	
+	public int getNumLetters(){
+		int answer = 0;
+		for(Sentence s: this.sentences){
+			answer += s.getNumLetter();
+		}
+	}
+	
+	public int getNoPunctuation(){
+		int answer = 0;
+		for(Sentence s : this.sentences){
+			answer+=s.getNoPunctuation();
+		}
+		return answer;
+	}
+	
 }
